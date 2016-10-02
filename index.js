506bb7de7b7528ce8cd574fd489ecdf93ca1bb4f
Clinton <iblamefish@gmail.com>
2016-10-02 18:53:47 +0100

    Big ol' refactor. Split up into modules, add promises

--- index.js
+++ index.js
@@ -1,72 +1,54 @@
-let cheerio = require("cheerio");
-let request = require("request");
+let Scraper = require("./scraper");
+let { get_next_topic, valid_links } = require("./util");
+
+const base = "https://en.wikipedia.org/wiki/";
+
+const target = "Philosophy";
 
 const chain = [];
 
-process_page = (error, response, html) => {
-  if (error) return console.log(error);
+let scraper = new Scraper({
+  base: base
+});
 
-  let dom = cheerio.load(html);
-
-  dom(".infobox, .metadata, .tright").remove();
-
-  let paragraphs = dom("#mw-content-text p").get();
-
-  let anchors = [];
-
-  paragraphs.forEach(function(p) {
-    let bracket_count = 0;
-
-    p.childNodes.reduce(function(acc, node) {
-      if (bracket_count === 0 && node.type === "tag" && node.name === "a") {
-        acc.push(node);
-      }
-
-      if (node.type === 'text') {
-
-        let left_parens = node.data.match(/\(/g);
-        let right_parens = node.data.match(/\)/g);
-
-        left_paren_count = left_parens ? left_parens.length : 0;
-        right_paren_count = right_parens ? right_parens.length : 0;
-
-        bracket_count = bracket_count + left_paren_count - right_paren_count;
-      }
-
-      return acc;
-    }, anchors);
-  });
-
-  if (anchors.length) {
-    let next_anchor = anchors[0];
-
-    let url = "https://en.wikipedia.org" + next_anchor.attribs.href;
-
-    if (chain.indexOf(url) !== -1) {
-      console.log("Stuck in a loop - there's no Philosophy to be found here");
-
-      return;
+function init() {
+  let topic = process.argv[2].split(" ").reduce(function (words, word) {
+    if (words.length === 0) {
+      words = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }
+    else {
+      words = words + "_" + word.toLowerCase();
     }
 
-    console.log("%d: %s => %s", chain.length + 1, next_anchor.children[0].data, url);
+    return words;
+  }, "");
 
-    chain.push(url);
+  console.log("Finding out how philosophical " + topic + " is");
 
-    scrape(url);
-  }
-  else {
-    console.log("Dead end :(");
-  }
+  visit_article(topic);
 }
 
-scrape = (url) => {
-  if (url === "https://en.wikipedia.org/wiki/Philosophy") {
-    console.log("Bingo! Got there in " + chain.length);
-
-    return true;
+function visit_article(topic) {
+  if (topic === target) {
+    return console.log("Success! Got there in " + chain.length);
   }
 
-  request(url, process_page);
+  scraper.request(topic)
+    .then((result) => valid_links(result.data))
+    .then((anchors) => {
+        return get_next_topic(anchors, chain)
+          .then((topic) => {
+            console.log("%d: %s => %s%s", chain.length + 1, topic.title, base, topic.url);
+
+            chain.push(topic.url);
+
+            return topic.url;
+          });
+    })
+    .then(visit_article)
+    .catch(function (message) {
+      console.warn(message);
+    });
 }
 
 if (process.argv.length !== 3) {
@@ -75,17 +57,6 @@ if (process.argv.length !== 3) {
   console.log("example: node index.js \"Nemesis (roller coaster)\"");
   return;
 }
-
-let topic = process.argv[2].split(" ").reduce(function (words, word) {
-  if (words.length === 0) {
-    words = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }
-  else {
-    words = words + "_" + word.toLowerCase();
-  }
-
-  return words;
-}, "");
-
-console.log("Finding out how philosophical " + topic + " is");
-scrape("https://en.wikipedia.org/wiki/" + topic);
+else {
+  init();
+}
---
+++ util.js
@@ -0,0 +1,69 @@
+let cheerio = require("cheerio");
+
+valid_links = (html) => {
+  return new Promise((resolve, reject) => {
+    let dom = cheerio.load(html);
+
+    dom(".infobox, .metadata, .tright").remove();
+
+    let paragraphs = dom("#mw-content-text p").get();
+
+    let anchors = [];
+
+    // Links are only valid if they are not within a pair of parens. It turns out
+    // that `html.replace(/(\([^)]+\))/g)` is a bit aggressive, so instead this
+    // counts the number of each open/close paren and keeps track of how many
+    // are currently open for each paragraph. If the count is 0, we know that
+    // the link is in a valid position.
+    paragraphs.forEach(function(p) {
+      let bracket_count = 0;
+
+      p.childNodes.forEach(function(node) {
+        if (bracket_count === 0 && node.type === "tag" && node.name === "a") {
+          anchors.push(node);
+        }
+
+        if (node.type === "text") {
+
+          let left_parens = node.data.match(/\(/g);
+          let right_parens = node.data.match(/\)/g);
+
+          left_paren_count = left_parens ? left_parens.length : 0;
+          right_paren_count = right_parens ? right_parens.length : 0;
+
+          bracket_count = bracket_count + left_paren_count - right_paren_count;
+        }
+      });
+
+      resolve(anchors);
+    });
+  });
+}
+
+function get_next_topic(anchors, previsited_anchors) {
+  return new Promise(function (resolve, reject) {
+    if (anchors.length === 0) {
+      reject("Dead end :(");
+
+      return;
+    }
+    else {
+      let next_anchor = anchors[0];
+
+      let url = next_anchor.attribs.href.replace(/^\/wiki\//, "");
+
+      if (previsited_anchors.indexOf(url) !== -1) {
+        reject("Stuck in a loop - there's no Philosophy to be found here");
+
+        return;
+      }
+
+      resolve({
+        title: next_anchor.children[0].data,
+        url: url
+      });
+    }
+  });
+}
+
+module.exports = { get_next_topic, valid_links }
